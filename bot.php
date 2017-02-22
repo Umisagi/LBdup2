67a6603dc728e56680307620ad61bc84147cd20b
Umisagi <zenchiter@gmail.com>
2017-02-23 02:26:39 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -62,6 +62,7 @@ if (!is_null($events['events'])) {
 				'replyToken' => $replyToken,
 				'messages' => [$messages]
 				];
+			}
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			
