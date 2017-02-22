1f31991024432eeec7cc0f4b2ba5a2113a9b829c
Umisagi <zenchiter@gmail.com>
2017-02-23 01:26:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -50,7 +50,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [['type' => 'text','text' => "ครับ"]],
+				'messages' => [$messages,['type' => 'text','text' => "ครับ"]],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
