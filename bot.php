878c24671a61c1bd6dd34f32243af816dd10663d
Umisagi <zenchiter@gmail.com>
2017-02-23 00:17:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -22,7 +22,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $response_message
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
