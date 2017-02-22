079eb39874932a24c5bfe662f6fc9f1aed201313
Umisagi <zenchiter@gmail.com>
2017-02-23 00:15:31 +0700

    Update chat.py

--- python/chat.py
+++ python/chat.py
@@ -5,5 +5,16 @@ if len(sys.argv) < 2:
   sys.exit(0)
 
 message = sys.argv[1]
-result = message
+
+if (message == "สวัสดี"):
+    result = "สวัสดีครับ มีอะไรให้รับใช้ครับ"
+elif (message == "สวัสดี ครับ"):
+    result = "สวัสดีครับ มีอะไรให้รับใช้ครับ"
+elif (message == "ลาก่อน"):
+    result = "ลาก่อนครับ"
+elif (message == "ควย"):
+    result = "ไม่น่ารักเลยครับ"
+else:
+    result = "ระบบไม่สามารถประมวลผลคำที่ท่านส่งมาได้ ขออภัยด้วยครับ"
+
 print ("%s" % result)
